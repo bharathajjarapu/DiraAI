8b35360089ad26ba9f9b4d03bb4137f44d998acc
Bharath <106143429+bharathajjarapu@users.noreply.github.com>
2024-06-09 23:57:22 +0530

    init

---
+++ app.py
@@ -0,0 +1,102 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+import google.generativeai as genai
+import streamlit as st
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=50000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def create_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("Faiss")
+
+def ingest_data():
+    pdf_files = [os.path.join("dataset", file) for file in os.listdir("dataset") if file.endswith(".pdf")]
+    raw_text = get_pdf_text(pdf_files)
+    text_chunks = get_text_chunks(raw_text)
+    create_vector_store(text_chunks)
+
+def get_conversational_chain():
+    prompt_template = """
+    You are Lawy a highly experienced attorney providing legal advice based on Indian laws. 
+    You will respond to the user's queries by leveraging your legal expertise and the Context Provided.
+    Context: {context}
+    Question: {question}
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash-latest", 
+        temperature=0.3, 
+        system_instruction="You are Lawy a highly experienced attorney providing legal advice based on Indian laws. You will respond to the user's queries by leveraging your legal expertise and the Context Provided.")
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    new_db = FAISS.load_local("Faiss", embeddings)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    return response["output_text"]
+
+def main():
+    st.set_page_config("Lawy", page_icon=":scales:")
+    st.header("Lawy: AI Lawyer :scales:")
+
+    if "data_ingested" not in st.session_state:
+        st.session_state.data_ingested = False
+
+    if not st.session_state.data_ingested:
+        st.write("Ingesting data, please wait...")
+        ingest_data()
+        st.session_state.data_ingested = True
+        st.rerun()
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hi I'm Lawy an AI Legal Advisor"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    prompt = st.chat_input("Type your question here...")
+
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = user_input(prompt)
+                    st.write(response)
+
+            if response is not None:
+                message = {"role": "assistant", "content": response}
+                st.session_state.messages.append(message)
+
+if __name__ == "__main__":
+    main()
