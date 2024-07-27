f33ed10945a0c8fdafeec096877de5b99da8c1d2
Bharath <itsbharathajjarapu@gmail.com>
2024-07-28 01:27:02 +0530

    hero

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def hero_section():
     """, unsafe_allow_html=True)
 
 def main():
+    hero_section()
     st.set_page_config("Dira AI", page_icon=":scales:")
     if "data_ingested" not in st.session_state:
         st.session_state.data_ingested = False
